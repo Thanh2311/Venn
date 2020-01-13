b79fcd6653f8a888c856c6e185dd41214fe6dbad
mah878 <mah11@my.yorku.ca>
2020-01-13 12:08:18 -0500

    created own branch

--- src/main/java/Venn/Main.java
+++ src/main/java/Venn/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("lab 0 testing");
+		System.out.println("created own branch");
 	}
 
 }
