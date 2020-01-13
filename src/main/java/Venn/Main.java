a8efbe5597f866a9668bd58b8011a917812c3114
mah878 <mah11@my.yorku.ca>
2020-01-13 12:12:06 -0500

    testing

--- src/main/java/Venn/Main.java
+++ src/main/java/Venn/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println("lab 0 testing");
 		System.out.println("created own branch");
+		System.out.println("Testing");
 	}
 
 }
