b83c98d36d5ff36a7fdb0c720a3aa49ee229b000
Thanh2311 <tcn1995@my.yorku.ca>
2020-01-20 12:34:44 -0500

    2nd test

--- src/main/java/Venn/Main.java
+++ src/main/java/Venn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("lab 0 testing");
-		System.out.println("branch/merge test ");
+		System.out.println("branch/merge test  evenennn more testing");
 	}
 
 }
