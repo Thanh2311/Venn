99f4c10225f8328c1e7bb99dd0204c3a923cf10d
Thanh2311 <tcn1995@my.yorku.ca>
2020-01-27 10:47:06 -0500

    Testing JavaFX

---
+++ src/main/java/Venn/JavaFX.java
@@ -0,0 +1,21 @@
+package Venn;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
+import javafx.scene.control.*;
+
+public class JavaFX extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		//primaryStage.setScene(s);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- src/main/java/Venn/Set.java
+++ src/main/java/Venn/Set.java
@@ -39,20 +39,6 @@ public class Set extends JPanel {
 		
 		textArea = new JTextArea();
 		panel.add(textArea);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new CompoundBorder(new LineBorder(new Color(240, 240, 240), 3), new LineBorder(new Color(0, 0, 0), 2)));
-		add(panel_1);
-		
-		JTextArea textArea_1 = new JTextArea();
-		panel_1.add(textArea_1);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new CompoundBorder(new LineBorder(new Color(240, 240, 240), 3), new LineBorder(new Color(0, 0, 0), 2)));
-		add(panel_2);
-		
-		JTextArea textArea_2 = new JTextArea();
-		panel_2.add(textArea_2);
 
 	}
 
