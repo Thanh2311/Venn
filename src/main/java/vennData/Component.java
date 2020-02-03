0e362d1cff792fa83f481a956bf0309202951157
DerryL7889 <randomcount@outlook.com>
2020-02-03 12:07:46 -0500

    comparable componenents

--- src/main/java/vennData/Component.java
+++ src/main/java/vennData/Component.java
@@ -1,6 +1,6 @@
 package vennData;
 
-public class Component {
+public class Component implements Comparable<Component>{
 	private String componentName;
 	private byte[] attributes;
 	
@@ -28,6 +28,11 @@ public class Component {
 			out += (b + ",");
 		}
 		return out;
+	}
+
+	@Override
+	public int compareTo(Component o) {
+		return this.getName().compareTo(o.getName());
 	}
 	
 }
--- src/main/java/vennData/ComponentList.java
+++ src/main/java/vennData/ComponentList.java
@@ -59,4 +59,8 @@ public class ComponentList {
 		}
 		return out;
 	}
+	
+	
 }
+
+
