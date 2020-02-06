d07f3a278b25aeeda604d8ed40a619255837c056
Thanh2311 <tcn1995@my.yorku.ca>
2020-02-05 21:27:05 -0500

    SelectSet Method Implemented
    addEleButton adds text to the circle selected by selectSet

--- src/main/java/Venn/FXController.java
+++ src/main/java/Venn/FXController.java
@@ -1,15 +1,16 @@
 package Venn;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
 
 public class FXController {
@@ -20,24 +21,32 @@ public class FXController {
 	@FXML private Pane newLabelPaneLeft;
 	@FXML private Pane newLabelPaneRight;
 	@FXML private Pane selectedPane;
+	//@FXML private Shape selectedShape;
 	
 	/*
-	 * This adds a new label to left set's vbox
+	 * Adds a new Label to the selected Sets VBox, default Left
 	 */	
 	@FXML public void addEleButton(ActionEvent event) {
 		
-		if (!textbox.getText().isEmpty()) {		
+		selectedPane = (selectedPane == null) ? newLabelPaneLeft : selectedPane;		
+		if (!textbox.getText().trim().isEmpty()) {		
 			Label lbl1 = new Label(textbox.getText());
-			lbl1.setAlignment(Pos.CENTER);
+			lbl1.setAlignment(Pos.CENTER_LEFT);
 			lbl1.setFont(new Font("Ariel Bold", 15));
-		    newLabelPaneLeft.getChildren().add(lbl1);
+		    selectedPane.getChildren().add(lbl1);
+		}
+		else {
+			textbox.setText(null);
 		}
 	}
-	
-	@FXML public void selectedSet(ActionEvent event) {
-		selectedPane = (Pane) event.getSource();
+
+	/* Selects a set by mouse click */
+	@FXML public void selectSet(MouseEvent event) {
 		
-		
-		
+		Pane x = (Pane) event.getSource();
+		selectedPane = (VBox) x.getChildren().get(1);	
+		Pane parentPane = (VBox) x.getParent();
+		Label selectedLabel = (Label)parentPane.getChildren().get(0);
+		System.out.println(selectedLabel.getText()+ " Selected");
 	}
 }
