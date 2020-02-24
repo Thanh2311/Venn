bdd31317454fa6e8db12d99d2039aee5fbaa8b3c
mah878 <mah11@my.yorku.ca>
2020-02-23 20:12:49 -0500

    coverage tester class

---
+++ src/main/java/Venn/FXControllerTester.java
@@ -0,0 +1,21 @@
+package Venn;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+public class FXControllerTester {
+
+	private FXController add;
+	private FXController rename;
+	
+	@BeforeEach 
+	public void setUp() throws Exception {
+		add = new FXController();
+	}
+	
+	@Test
+	public void testRename() {
+		}
+}
