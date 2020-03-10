8fd31902192ee38c36c2859cd8747bc0d83a275a
mah878 <mah11@my.yorku.ca>
2020-03-10 10:15:13 -0400

    k

--- src/main/java/Venn/ApplicationWindow.java
+++ src/main/java/Venn/ApplicationWindow.java
@@ -8,9 +8,9 @@ import javafx.scene.layout.*;
 
 public class ApplicationWindow extends Application {
 
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
