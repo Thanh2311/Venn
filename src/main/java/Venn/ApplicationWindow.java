8e4c32fe50aeb395784dce489528fc6911f78c1e
Thanh2311 <tcn1995@my.yorku.ca>
2020-03-27 02:43:37 -0400

    Improvement on resizing

--- src/main/java/Venn/ApplicationWindow.java
+++ src/main/java/Venn/ApplicationWindow.java
@@ -9,7 +9,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 
 public class ApplicationWindow extends Application {
-
+	
+	double width = 1280;
+	double height = 720;
+	
 //	public static void main(String[] args) {
 //		launch(args);
 //	}
@@ -20,23 +23,28 @@ public class ApplicationWindow extends Application {
 			ScrollPane root = (ScrollPane)FXMLLoader.load(getClass().getResource("FX.fxml"));
 			AnchorPane fakeRoot = (AnchorPane)root.getContent();
 			BorderPane border = (BorderPane)fakeRoot.getChildren().get(1);
-			Scene scene = new Scene(root,1280,720);
-			border.prefWidthProperty().bind(primaryStage.widthProperty());
-			border.prefHeightProperty().bind(primaryStage.heightProperty());
+			Scene scene = new Scene(root,width,height);
+			border.prefWidthProperty().bind(scene.widthProperty());
+			border.prefHeightProperty().bind(scene.heightProperty());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Venn Diagram Example");
 			primaryStage.setMinHeight(650);
 			primaryStage.setMinWidth(850);
 
+			double minHeight = fakeRoot.getMinHeight();
 			Pane input = (Pane)border.getLeft();
 			primaryStage.widthProperty().addListener((observed, old, newV) -> {
-						if ( newV.doubleValue() < 1100) {
+						if ( newV.doubleValue() < 1020) {
 							border.setLeft(null);
-							border.setBottom(input);
+							border.setBottom(input);			
+							fakeRoot.setMinHeight(minHeight);
 						}
 						else {
 							border.setBottom(null);
 							border.setLeft(input);
+							//fakeRoot.setMaxHeight(0);
+							fakeRoot.setMinHeight(height);
+						//	fakeRoot.setPrefHeight(0);
 						}
 			});
 	;		primaryStage.show();
